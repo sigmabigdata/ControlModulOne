0458bfaad1f4bb45c53596e088dc40cbbfd384cf
anatoli dyrul <dyrulan@gmail.com>
2023-02-22 20:20:53 +0300

    Mas add

--- Program.cs
+++ Program.cs
@@ -2,8 +2,18 @@
 string[] arrayFinal = new string[arrayOrigial.Length];
 
 
+int count = 0;
+    
+    for (int i = 0; i < arrayOrigial.Length; i++)
+    {
+    if(arrayOrigial[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayOrigial[i];
+        count++;
+        }
+    }
 
-  
+
 
 
 
